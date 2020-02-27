4873eeab97690f70e92b4df9cc02ab8951fbaab5
Elias <elias.berger@tum.de>
2020-02-27 23:13:44 +0100

    Works with fixed for my categories, not in general

--- Main.py
+++ Main.py
@@ -5,35 +5,50 @@ import pandas as pd
 
 data = pd.read_csv("MoneyCoach.csv", delimiter=';', header=0)
 data['Amount'] = data['Amount'].apply(lambda x : x.replace('.','').replace(',','.'))
-print(data['Amount'])
 data['Amount'] = data['Amount'].astype('float')
 incomes = data[data['Type'] == 'Income']
 expenses = data[data['Type'] == 'Expense']
+
+# create array with income types and sum per type
 income_types = incomes['Category'].unique()
 income_type_sum = []
-
 for it in income_types:
     income_per_type = incomes[incomes['Category'] == it]
-    print(income_per_type['Amount'])
-    income_type_sum.append(income_per_type['Amount'].sum())
+    income_type_sum.append(round(income_per_type['Amount'].sum(), 2))
 
-print(income_type_sum)
-print(income_types)
+# create array with expense types and sum per type
+expense_types = expenses['Category'].unique()
+expense_type_sum = []
+for et in expense_types:
+    expense_per_type = expenses[expenses['Category'] == et]
+    expense_type_sum.append(round(expense_per_type['Amount'].sum(), 2))
 
-all_labels = income_types + ['Budget']
+# compute amount saved during period
+savings = round(sum(income_type_sum) - sum(expense_type_sum), 2)
+
+all_labels = np.concatenate((income_types, ["Budget"], expense_types, ["Savings"]), axis=0).tolist()
+source_index = np.concatenate((np.linspace(0,5,6,dtype=int), [6]*(len(expense_types)+2)), axis=0).tolist()
+target_index = [6]*len(income_types) + [6] + (np.linspace(len(income_types)+1, len(income_types)+len(expense_types)+1, len(expense_types)+1, dtype=int).tolist())
+value = income_type_sum + [0] + expense_type_sum + [savings]
+
+print(len(all_labels), len(source_index), len(target_index))
+for i in range(len(all_labels)):
+    print(source_index[i], all_labels[source_index[i]])
+    print(target_index[i], all_labels[target_index[i]])
+    print(value[i])
 
 fig = go.Figure(data=[go.Sankey(
     node = dict(
     pad = 15,
     thickness = 20,
     line = dict(color = "black", width = 0.5),
-    label = np.asarray(income_types),
+    label = np.asarray(all_labels),
     color = "blue"
     ),
     link = dict(
-    source = [0,1,2,3,4], # indices correspond to labels, eg A1, A2, A2, B1, ...
-    target = [5,5,5,5,5],
-    value = np.asarray(income_type_sum)
+    source = source_index, # indices correspond to labels, eg A1, A2, A2, B1, ...
+    target = target_index,
+    value = value
   ))])
 
 fig.update_layout(title_text="Basic Sankey Diagram", font_size=10)
